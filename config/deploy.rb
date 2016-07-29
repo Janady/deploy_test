66fbde7e10c096ea20ed92e98133dea42235b849
janady <zhengyk3@gmail.com>
2016-07-29 19:22:56 +0800

    third

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,8 @@ set :rbenv_roles, :all # default value
 set :deploy_to, '/home/janady/deploy'
 
 # If the environment differs from the stage name
-set :rails_env, 'staging'
+# set :rails_env, 'staging'
+set :rails_env, "production"
 
 # Defaults to :db role
 set :migration_role, :db
@@ -71,3 +72,28 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :deploy do
+
+  namespace :assets do
+    task :precompile do
+      on roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :rake, "assets:precompile RAILS_ENV=production"
+          end
+        end
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
